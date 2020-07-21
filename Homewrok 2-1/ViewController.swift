4fd413d39118d897ad8874f0abbc31e3235175f5
BryanKuo <bryan0011@gmail.com>
2020-07-21 15:52:11 +0800

    Initial Commit

---
+++ Homewrok 2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homewrok 2-1
+//
+//  Created by Bryan Kuo on 2020/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
